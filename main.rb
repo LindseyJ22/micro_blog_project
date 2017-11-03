f1eb48d2dd554545818d1d283ea20d8e8bd3d612
Lindsey Johnson <redhook@hotmail.com>
2017-11-03 15:24:21 -0600

    in main.rb I routed the home page to the sign up page then copied a form from bootsrap and pasted into my sign_up.erb page and styled it with a background image and rearranged some of the posistioning in css

--- main.rb
+++ main.rb
@@ -2,7 +2,23 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './models'
 set :database, "sqlite3:main.sqlite3"
+set :sessions, true
+
 get '/' do
+	@users = User.all
+	@posts = Post.all
 	erb :home
+end
+
+get '/sign_up' do
+	erb :sign_up
+end
+
+post '/sign_up' do
+	@name = params[:name]
+	@user = User.create(params[:user])
+	p params
+	session[:user_id] = @user.id
+	redirect '/'
 end
 		
